56cc77da04b370c36d907454a577af36f90c04da
ben <bebenbarnet@gmail.com>
2023-07-02 10:06:58 +0700

    Category UI Update

--- app/src/main/java/com/bennohan/mynotes/ui/categoryNote/CategoryActivity.kt
+++ app/src/main/java/com/bennohan/mynotes/ui/categoryNote/CategoryActivity.kt
@@ -1,15 +1,61 @@
 package com.bennohan.mynotes.ui.categoryNote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.bennohan.mynotes.R
 import com.bennohan.mynotes.base.BaseActivity
+import com.bennohan.mynotes.database.Categories
+import com.bennohan.mynotes.database.Const
+import com.bennohan.mynotes.database.Note
 import com.bennohan.mynotes.databinding.ActivityCategoryBinding
+import com.bennohan.mynotes.databinding.ItemNoteBinding
+import com.bennohan.mynotes.ui.addNote.NoteActivity
+import com.bennohan.mynotes.ui.home.NavigationActivity
+import com.crocodic.core.base.adapter.ReactiveListAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class CategoryActivity : BaseActivity<ActivityCategoryBinding,CategoryViewModel>(R.layout.activity_category) {
+class CategoryActivity :
+    BaseActivity<ActivityCategoryBinding, CategoryViewModel>(R.layout.activity_category) {
+
+    private var dataCategories = ArrayList<Categories?>()
+
+    private val adapterCategory by lazy {
+        ReactiveListAdapter<ItemNoteBinding, Categories>(R.layout.item_category).initItem { position, data ->
+        }
+
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        observe()
+
+        binding.rvCategory.adapter = adapterCategory
+
+    }
+
+    private fun observe() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel.listCategory.collect { data ->
+                        dataCategories.addAll(data)
+                        adapterCategory.submitList(data)
+                        Log.d("cek categories", dataCategories.toString())
+                        Log.d("cek it", data.toString())
+
+                    }
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/com/bennohan/mynotes/ui/categoryNote/CategoryViewModel.kt
+++ app/src/main/java/com/bennohan/mynotes/ui/categoryNote/CategoryViewModel.kt
@@ -1,10 +1,22 @@
 package com.bennohan.mynotes.ui.categoryNote
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
 import com.bennohan.mynotes.api.ApiService
 import com.bennohan.mynotes.base.BaseViewModel
+import com.bennohan.mynotes.database.Categories
 import com.bennohan.mynotes.database.UserDao
+import com.crocodic.core.api.ApiCode
+import com.crocodic.core.api.ApiObserver
+import com.crocodic.core.api.ApiResponse
+import com.crocodic.core.extension.toList
+import com.crocodic.core.extension.toObject
 import com.google.gson.Gson
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,4 +25,31 @@ class CategoryViewModel  @Inject constructor(
     private val gson: Gson,
     private val userDao: UserDao
 ) : BaseViewModel() {
+
+    private var _listCategory = MutableSharedFlow<List<Categories?>>()
+    var listCategory = _listCategory.asSharedFlow()
+
+
+    fun getCategory(
+    ) = viewModelScope.launch {
+        _apiResponse.emit(ApiResponse().responseLoading())
+        ApiObserver(
+            { apiService.categoryList() },
+            false,
+            object : ApiObserver.ResponseListener {
+                override suspend fun onSuccess(response: JSONObject) {
+                    val data = response.getJSONArray(ApiCode.DATA).toList<Categories>(gson)
+                    Log.d("cek list category",data.toString())
+                    _listCategory.emit(data)
+                    _apiResponse.emit(ApiResponse().responseSuccess())
+                }
+
+                override suspend fun onError(response: ApiResponse) {
+                    //Ask why the response wont show if the super is gone
+                    super.onError(response)
+                    _apiResponse.emit(ApiResponse().responseError())
+                }
+            })
+    }
+    
 }
